be147d0637ae3083e25c27d7938f1aab1c7a9378
Clement LABBE <clementlabbe@MacBook-Pro-de-Clement.local>
2020-04-21 08:29:42 -0400

    initial commit.

---
+++ TP5_AppliOrienteeServices/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TP5_AppliOrienteeServices
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
