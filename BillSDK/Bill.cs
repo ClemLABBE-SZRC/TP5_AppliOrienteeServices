49006fe5b69d84deb3fbeab97f89cbc1e13faa11
ffcfalcos <thomas.beauchataud@yahoo.fr>
2020-04-27 12:42:13 -0400

    Implementation de Bill & BillLine

--- BillSDK/Bill.cs
+++ BillSDK/Bill.cs
@@ -4,5 +4,33 @@ namespace BillSDK
 {
     public class Bill
     {
+        private static float taxe = 0.1;
+
+        private User user;
+        private List<BillLine> billLines;
+        private int totalHt;
+        private int totalTtc;
+
+        public Bill(User user, List<BillLine> billLines)
+        {
+            this.user = user;
+            this.billLines = billLines;
+            this.totalHt = 0;
+            foreach(BillLine billLine in billLines)
+            {
+                this.totalHt += billLine.Total;
+            }
+            this.totalTtc = this.totalHt * (1 + taxe);
+        }
+
+        public static Bill CreateBill(User user, List<ItemLine> lines)
+        {
+            List<BillLine> billLines = new List<BillLine>();
+            foreach(ItemLine line in lines)
+            {
+                billLines.Add(new BillLine(line.Item, line.Quantity));
+            }
+            return new Bill(user, billLines);
+        }
     }
 }
--- BillSDK/BillLine.cs
+++ BillSDK/BillLine.cs
@@ -2,7 +2,21 @@ using System;
 
 namespace BillSDK
 {
-    class BillLine
+    public class BillLine
     {
+        private Item item;
+        private int quantity;
+        private int total;
+
+        public BillLine(Item item, int quantity)
+        {
+            this.item = item;
+            this.quantity = quantity;
+            this.total = item.Price * quantity;
+        }
+
+        public Item Item { get => item; }
+        public int Quantity { get => quantity; }
+        public int Total { get => total; }
     }
 }
