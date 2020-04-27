bbf569409a3762a663af7f168a472cf00e9dde26
QuentinLetort <quentin.letort@devinci.fr>
2020-04-27 13:21:24 -0400

    add stock service

--- StockSDK/Item.cs
+++ StockSDK/Item.cs
@@ -4,5 +4,17 @@ namespace StockSDK
 {
     public class Item
     {
+        public string Name { get; }
+        public float UnitPrice { get; set; }
+
+        public Item(string name, float unitPrice)
+        {
+            this.Name = name;
+            this.UnitPrice = unitPrice;
+        }
+        public override string ToString()
+        {
+            return $"{Name}, {UnitPrice}";
+        }
     }
 }
--- StockSDK/StockManager.cs
+++ StockSDK/StockManager.cs
@@ -1,9 +1,29 @@
 using System;
+using RPC;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace StockSDK
 {
     public class StockManager
     {
-
+        public ItemLine ReserveItem(int quantity, string name)
+        {
+            RPCClient rpcClient = new RPCClient();
+            JObject message = new JObject(){
+                {"quantity", quantity},
+                {"name", name}
+            };
+            string response = rpcClient.Call(message.ToString(), "stock_queue");
+            rpcClient.Close();
+            return JsonConvert.DeserializeObject<ItemLine>(response);
+        }
+        public bool ReleaseItem(ItemLine line)
+        {
+            RPCClient rpcClient = new RPCClient();
+            string response = rpcClient.Call(JsonConvert.SerializeObject(line), "stock_queue");
+            rpcClient.Close();
+            return Boolean.Parse(response);
+        }
     }
 }
